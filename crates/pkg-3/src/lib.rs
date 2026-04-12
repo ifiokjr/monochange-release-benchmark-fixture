ad8bb1a32cb9c900402d489d3a304703461bde5a
fixture <fixture@example.com>
2026-04-13 00:44:48 +0100

    feat: release fixture commit 4.2

--- crates/pkg-3/src/lib.rs
+++ crates/pkg-3/src/lib.rs
@@ -117,3 +117,7 @@ pub fn history_commit_220_3() -> &'static str {
 pub fn release_pr_4_commit_1_3() -> &'static str {
 	"release_pr_4_commit_1"
 }
+
+pub fn release_pr_4_commit_2_3() -> &'static str {
+	"release_pr_4_commit_2"
+}
