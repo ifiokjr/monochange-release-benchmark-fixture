7594cd2b86657ca89efd932c2a606ed9fc99d118
fixture <fixture@example.com>
2026-04-13 00:44:20 +0100

    chore: history commit 170

--- crates/pkg-1/src/lib.rs
+++ crates/pkg-1/src/lib.rs
@@ -85,3 +85,7 @@ pub fn history_commit_154_1() -> &'static str {
 pub fn history_commit_162_1() -> &'static str {
 	"history_commit_162"
 }
+
+pub fn history_commit_170_1() -> &'static str {
+	"history_commit_170"
+}
